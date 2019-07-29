8759360f6cbd01d0b28675920ecf73d778b78d85
Alexander Riccio <test35965+learn@gmail.com>
2019-07-29 15:22:35 -0400

    add fetch after we configured rails

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,11 @@
 
+
+function mapClick(event) {
+    const body = {
+        latitude: event.latLng.lat(),
+        longitude: event.latLng.lng()
+    };
+    fetch(`http://localhost:3000/stations/${btoa(JSON.stringify(body))}`)
+    
+}
+
