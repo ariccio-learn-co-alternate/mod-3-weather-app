660596bbbb8c0e53c50bc6d5a2c4db9cffe64a15
Alexander Riccio <test35965+learn@gmail.com>
2019-07-29 15:55:58 -0400

    I didn't merge that?

--- frontend/index.js
+++ frontend/index.js
@@ -83,13 +83,3 @@ function mapClick(event) {
     
 }
 
-
-function mapClick(event) {
-    const body = {
-        latitude: event.latLng.lat(),
-        longitude: event.latLng.lng()
-    };
-    fetch(`http://localhost:3000/stations/${btoa(JSON.stringify(body))}`)
-    
-}
-
