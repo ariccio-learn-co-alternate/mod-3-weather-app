db390c74daf749a286880ccd3f951e1f6b73047b
eschechter <schechter.evan@gmail.com>
2019-07-30 10:21:41 -0400

    fix route for weather

--- backend/weather/config/routes.rb
+++ backend/weather/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/weather/:noaa_id/:year", to: "stations#query_weather"
+  get "/weather/:noaa_id_year", to: "stations#query_weather"
   get "/stations/:latlong", to: "stations#query_station"
 end
