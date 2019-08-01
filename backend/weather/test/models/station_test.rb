15c57b9fbd31339e6f69caa09a9b59d9acfad6f6
Alexander Riccio <test35965+learn@gmail.com>
2019-08-01 14:53:07 -0400

    new tests for daily data

--- backend/weather/test/models/station_test.rb
+++ backend/weather/test/models/station_test.rb
@@ -6,7 +6,7 @@ class StationTest < ActiveSupport::TestCase
   # end
   Rake::Task["db:seed"].invoke # seed after starting
   test 'right number of months' do
-    assert MONTHS.length == 12
+    assert Station.first.build_base_monthly_hash.length == 12
   end
 
   test 'opposite point on equator has a haversine distance of PI, thanks evan for math brainz' do
@@ -27,11 +27,15 @@ class StationTest < ActiveSupport::TestCase
     assert Station.haversine_distance(0,0, -180, -180) == 0.0
   end
 
-  test 'Station::get_weather(year) returns reasonable data' do
+  test 'Station::get_monthly_weather(year) returns reasonable data' do
     assert Station.count > 0, 'Please seed the database.'
-    result = Station.first.get_weather(1950)
+    result = Station.first.get_monthly_weather(1950)
     assert result.has_key?('meta')
     assert result['meta'].has_key?('year')
+    assert result['meta']['year'] > 0
+
+    # 81 years of forward compatibility
+    assert result['meta']['year'] < 2100
 
     assert result['meta'].has_key?('noaa_id')
     assert !(result['meta']['noaa_id'].empty?)
@@ -46,19 +50,72 @@ class StationTest < ActiveSupport::TestCase
     assert result['results'].length == 12
     
     assert result['results'] != nil
+
     result['results'].each do |single_result|
       assert single_result.has_key?(:month)
       assert !(single_result[:month].empty?)
 
       assert single_result.has_key?('mean_temp')
       # absolute zero is minus 459.67 degrees Fahrenheit
-      assert single_result['mean_temp'] > -460 
+      assert single_result['mean_temp'] > -460
+
+      # ~melting point of lead is 621.4°F
+      assert single_result['mean_temp'] < 621
 
       assert single_result.has_key?('total_precip')
       assert single_result['total_precip'] > -0.1
 
       assert single_result.has_key?('total_snow')
       assert single_result['total_snow'] > -0.1
+    end
+
+  end
+
+  test 'Station::get_daily_weather(year, month) returns reasonable data' do
+    result = Station.first.get_daily_weather(1950, 1)
+    assert result.has_key?('meta')
+
+    assert result['meta'].has_key?('year')
+    assert result['meta']['year'] > 0
+
+    # 81 years of forward compatibility
+    assert result['meta']['year'] < 2100
+
+    assert result['meta'].has_key?('month')
+    assert result['meta']['month'] >= 0
+    assert result['meta']['month'] <= 12
+
+    assert result['meta'].has_key?('noaa_id')
+    assert !(result['meta']['noaa_id'].empty?)
+
+    assert result['meta'].has_key?('city')
+    assert !(result['meta']['city'].empty?)
+
+    assert result['meta'].has_key?('state')
+    assert !(result['meta']['state'].empty?)
+
+    assert result.has_key?('results')
+    
+    assert result['results'] != nil
+    assert result['results'].length > 27
+
+    result['results'].each do |single_result|
+      assert single_result.has_key?(:day)
+
+      assert single_result.has_key?('max_temp')
+      # absolute zero is minus 459.67 degrees Fahrenheit
+      assert single_result['max_temp'] > -460
+
+      # ~melting point of lead is 621.4°F
+      assert single_result['max_temp'] < 621
+
+      assert single_result.has_key?('min_temp')
+      # absolute zero is minus 459.67 degrees Fahrenheit
+      assert single_result['min_temp'] > -460
+
+      # ~melting point of lead is 621.4°F
+      assert single_result['min_temp'] < 621
+
     end
 
   end
