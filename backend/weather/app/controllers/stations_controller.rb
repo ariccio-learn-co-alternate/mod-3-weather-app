681e172b1a21680285a48e5ba8313ef44ba3c73d
Alexander Riccio <test35965+learn@gmail.com>
2019-08-02 10:48:26 -0400

    fix a bunch of style warnings

--- backend/weather/app/controllers/stations_controller.rb
+++ backend/weather/app/controllers/stations_controller.rb
@@ -1,18 +1,18 @@
 class StationsController < ApplicationController
   def query_station
     latlong = JSON.parse(Base64.urlsafe_decode64(params[:latlong]))
-    render json: Station.closest_station(latlong["latitude"], latlong["longitude"])
+    render json: Station.closest_station(latlong['latitude'], latlong['longitude'])
   end
 
   def query_monthly_weather
     noaa_id_year = JSON.parse(Base64.urlsafe_decode64(params[:noaa_id_year]))
-    station = Station.find_by(noaa_id: noaa_id_year["noaa_id"])
-    render json: station.get_monthly_weather(noaa_id_year["year"])
+    station = Station.find_by(noaa_id: noaa_id_year['noaa_id'])
+    render json: station.get_monthly_weather(noaa_id_year['year'])
   end
 
   def query_daily_weather
     noaa_id_year_month = JSON.parse(Base64.urlsafe_decode64(params[:noaa_id_year_month]))
-    station = Station.find_by(noaa_id: noaa_id_year_month["noaa_id"])
-    render json: station.get_daily_weather(noaa_id_year_month["year"].to_i, noaa_id_year_month["month"].to_i)
+    station = Station.find_by(noaa_id: noaa_id_year_month['noaa_id'])
+    render json: station.get_daily_weather(noaa_id_year_month['year'].to_i, noaa_id_year_month['month'].to_i)
   end
 end
